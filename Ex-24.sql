3ec219249c3189e65e99183df1dea2ced2ba17c9
rune7536 <rune7536edu.eal.dk>
2018-11-14 09:52:17 +0100

    inserted data

---
+++ Ex-24.sql
@@ -0,0 +1,54 @@
+INSERT INTO Pet_Owner (OwnerLastName, OwnerFirstName, OwnerPhone, OwnerEmail)VALUES
+(
+'Downs', 'Marsha', '5555378765', 'Marsha.Downs@somewhere.com'
+);
+
+INSERT INTO Pet_Owner (OwnerLastName, OwnerFirstName, OwnerPhone, OwnerEmail)VALUES
+(
+'James', 'Richard', '5555377654', 'Richard.James@somewhere.som'
+);
+
+INSERT INTO Pet_Owner (OwnerLastName, OwnerFirstName, OwnerPhone, OwnerEmail)VALUES
+(
+'Frier', 'Liz', '5555376543', 'Liz.Frier@somewhere.com'
+);
+
+INSERT INTO Pet_Owner (OwnerLastName, OwnerFirstName, OwnerPhone, OwnerEmail)VALUES
+(
+'Trent', 'Miles', ' ', 'Miles.Trent@somewhere.com'
+);
+
+INSERT INTO Pet VALUES
+(
+1, 'King', 'Dog', 'Std. Poodle', '21.02.2011', 25.5, 1
+);
+
+INSERT INTO Pet VALUES
+(
+2, 'Teddy', 'Cat', 'Cashmere', '01.02.2012', 10.5, 2
+);
+
+INSERT INTO Pet VALUES
+(
+3, 'Fido', 'Dog', 'Std. Poodle', '17.07.2010', 28.5, 1
+);
+
+INSERT INTO Pet VALUES
+(
+4, 'AJ', 'Dog', 'Collie Mix', '05.05.2011', 20.0, 3
+);
+
+INSERT INTO Pet VALUES
+(
+5, 'Cedro', 'Cat', 'Unknown', '06.06.2009', 9.5, 2
+);
+
+INSERT INTO Pet VALUES
+(
+6, 'Wooley', 'Cat', 'Unknown', ' ', 9.5, 2
+);
+
+INSERT INTO Pet VALUES
+(
+7, 'Buster', 'Dog', 'Border Collie', '11.12.2008', 25.0, 4
+);
