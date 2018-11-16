d52d1a547170e7ac253d5761bd4e9f791833936a
rune7536 <rune7536edu.eal.dk>
2018-11-16 10:36:57 +0100

    done with ex24

--- Ex-24.sql
+++ Ex-24.sql
@@ -1,3 +1,6 @@
+USE A_DB29_2018
+
+-- DC s. 214
 INSERT INTO Pet_Owner (OwnerLastName, OwnerFirstName, OwnerPhone, OwnerEmail)VALUES
 (
 'Downs', 'Marsha', '5555378765', 'Marsha.Downs@somewhere.com'
@@ -128,9 +131,92 @@ HAVING		COUNT (*) > 1;
 -- øvelse 3.35
 SELECT	OwnerLastName, OwnerFirstName, OwnerEmail
 FROM	Pet_Owner
-Where	OwnerId IN 
+WHERE	OwnerId IN 
 		(SELECT		OwnerId
 		 FROM		Pet
 		 WHERE		PetType = 'Cat');
 
 -- øvelse 3.36
+SELECT	OwnerLastName, OwnerFirstName, OwnerEmail
+FROM	Pet_Owner
+WHERE	OwnerId IN 
+		(SELECT		OwnerId
+		 FROM		Pet
+		 WHERE		PetName = 'Teddy');
+
+-- øvelse 3.37
+CREATE TABLE Breed
+(
+BreedName				NVARCHAR(50)		NOT NULL,
+MinWeight				FLOAT				NOT NULL,
+MaxWeight				FLOAT				NOT NULL,
+AverageLifeExpectancy	INT					NOT NULL,
+CONSTRAINT Breed_PK PRIMARY KEY (BreedName)
+);
+
+INSERT INTO Breed VALUES
+(
+'Border Collie', 15.0, 22.5, 20
+);
+INSERT INTO Breed VALUES
+(
+'Cashmere', 10.0, 15.0, 12
+);
+INSERT INTO Breed VALUES
+(
+'Collie Mix', 17.5, 25.0, 18
+);
+INSERT INTO Breed VALUES
+(
+'Std. Poodle', 22.5, 30.0, 18
+);
+INSERT INTO Breed VALUES
+(
+'Unknown', ' ', ' ', ' '
+);
+
+ALTER TABLE Pet
+ADD CONSTRAINT Pet_Breed_FK Foreign KEY (PetBreed)
+	REFERENCES Breed (BreedName) 
+		ON UPDATE CASCADE
+
+SELECT	OwnerLastName, OwnerFirstName, OwnerEmail
+FROM	Pet_Owner
+WHERE	OwnerId IN 
+		(SELECT		OwnerId
+		 FROM		Pet
+		 WHERE		PetBreed IN 
+					(SELECT		BreedName
+					 FROM		Breed
+					 WHERE		AverageLifeExpectancy > 15)
+		);
+
+-- øvelse 3.38
+SELECT	OwnerLastName, OwnerFirstName, OwnerEmail
+FROM	Pet_Owner JOIN Pet
+		ON		Pet_Owner.OwnerId = 
+					Pet.OwnerId
+WHERE	PetType = 'Cat'
+
+-- øvelse 3.39
+SELECT	OwnerLastName, OwnerFirstName, OwnerEmail
+FROM	Pet_Owner JOIN Pet
+		ON		Pet_Owner.OwnerId =
+					Pet.OwnerId
+WHERE	PetName = 'Teddy'
+
+-- øvelse 3.40
+SELECT	OwnerLastName, OwnerFirstName, OwnerEmail
+FROM	Pet_Owner AS PO JOIN Pet AS P
+		ON		PO.OwnerId = P.OwnerId
+				JOIN Breed AS B
+					ON P.PetBreed = B.BreedName
+WHERE	AverageLifeExpectancy > 15
+
+-- øvelse 3.41
+SELECT	OwnerLastName, OwnerFirstName, PetName, PetType, PetBreed, AverageLifeExpectancy
+FROM	Pet_Owner AS PO JOIN Pet AS P
+		ON		PO.OwnerId = P.OwnerId
+				JOIN Breed AS B
+					ON P.PetBreed = B.BreedName
+WHERE	BreedName != 'Unknown'
